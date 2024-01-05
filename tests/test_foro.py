5a63e2be23102dc7b3e555bcc978d637133cd103
MRiveraGuerrero <104358538+MRiveraGuerrero@users.noreply.github.com>
2024-01-05 17:20:04 +0100

    test que si funcinonan

--- tests/test_foro.py
+++ tests/test_foro.py
@@ -1,22 +1,27 @@
 from . import BaseTestClass
 from bs4 import BeautifulSoup
-
 class TestForo(BaseTestClass):
 
-    def test_foro_with_text(self):
+    def test_foro_with_user(self):
+        # Comrpobamos que salga el campo para añadir un tema
         self.login('james@gmail.com', '123456')
-
-
-        res = self.client.get('/foro?texto=test')
-        page = BeautifulSoup(res.data, features="html.parser")
-
-        self.assertEqual('Título del Tema', page.find_all('h2')[0].get_text())
-
-
-    def test_foro_without_text(self):
-
         res = self.client.get('/foro')
         page = BeautifulSoup(res.data, features="html.parser")
 
+        self.assertEqual('Tema', page.find_all('label')[0].get_text())
 
-        self.assertEqual('Título del Tema', page.find_all('h2')[0].get_text())
+
+    def test_foro_without_user(self):
+        # Comprobamos que no salga el campo de rellenar
+        self.login('james@gmail.com', '0')
+        res = self.client.get('/foro')
+        page = BeautifulSoup(res.data, features="html.parser")
+
+        self.assertNotEqual('Tema', page.find_all('label')[0].get_text())
+
+    def test_foro_se_imprimen_los_temas(self):
+        self.login('james@gmail.com', '123456')
+        res = self.client.get('/foro')
+        page = BeautifulSoup(res.data, features="html.parser")
+        self.assertEqual('Accion', page.find_all('h5')[0].get_text())
+        self.assertEqual('España', page.find_all('h5')[2].get_text())
--- tests/test_post_crear.py
+++ tests/test_post_crear.py
@@ -4,22 +4,27 @@ from bs4 import BeautifulSoup
 
 class TestPostCrear(BaseTestClass):
 
-    def test_post_crear_authenticated(self):
+    def test_crear_post_authenticated(self):
+        # Logueamos al usuario
         self.login('james@gmail.com', '123456')
 
-        cod_tema = 1
-        res = self.client.post(f'/post/{cod_tema}/crearComentario', data={'contenido': 'Nuevo comentario'})
+        # Accedemos a la página del post
+        res = self.client.get('/post/2')  # Asegúrate de ajustar el ID del post según tu caso
+        page = BeautifulSoup(res.data, features="html.parser")
 
+        # Comprobamos que hay un campo para añadir un comentario
+        self.assertEqual('Publicar comentario:', page.find_all('label')[0].get_text())
 
-        self.assertEqual(302, res.status_code)
+        # Contamos los comentarios actuales
+        num_comentarios = len(page.find_all('h5', class_='mt-0 font-weight-bold'))
 
+        # Creamos un comentario
+        self.client.post('/post/2/crearComentario', data={'contenido': 'Este es un comentario de prueba'})
 
+        # Volvemos a acceder a la página del post
+        res_after = self.client.get('/post/2')  # Asegúrate de ajustar el ID del post según tu caso
+        page_after = BeautifulSoup(res_after.data, features="html.parser")
 
-    def test_post_crear_unauthenticated(self):
-
-        cod_tema = 1
-        res = self.client.post(f'/post/{cod_tema}/crearComentario', data={'contenido': 'Nuevo comentario'})
-
-
-        self.assertEqual(302, res.status_code)
+        # Comprobamos que el número de comentarios ha aumentado en 1
+        self.assertEqual(num_comentarios + 1, len(page_after.find_all('h5', class_='mt-0 font-weight-bold')))
 
