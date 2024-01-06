7b15117c9622a03c5f44739029efd42ecaa72b98
unax26203 <unaxzardoya@gmail.com>
2024-01-06 13:44:44 +0100

    tests hechos

---
+++ tests/test_eliminaramigo.py
@@ -0,0 +1,25 @@
+from . import BaseTestClass
+class TestGestorUsuarios(BaseTestClass):
+
+
+    def test_eliminar_amigo(self):
+        receptor_username = "usuario1"
+        solicitante_username = "usuario2"
+
+        self.login("usuario1@gmail.com","usuario1")
+
+
+        self.db.insert("INSERT INTO Amigo (usuarioA, usuarioB) VALUES (?, ?)",
+                  (receptor_username, solicitante_username))
+
+        # Realizar la acción que se está probando
+        res = self.client.post('/eliminar_amigo',data={'amigo_username': solicitante_username})
+
+        # Verificar que la relacion de amistad se haya eliminado
+        existing_request = self.db.select("SELECT * FROM Amigo WHERE usuarioA = ? AND usuarioB = ?",
+                                     (receptor_username, solicitante_username))
+        self.assertEqual(0, len(existing_request))
+
+        # Verificar redirección
+        self.assertEqual(res.status_code, 302)  # Código de estado de redirección
+        self.assertEqual(res.location, '/amigos.html')  # Dirección de redirección esperada
--- tests/test_rechazarsolicitud.py
+++ tests/test_rechazarsolicitud.py
@@ -2,7 +2,7 @@ from . import BaseTestClass
 class TestGestorUsuarios(BaseTestClass):
 
 
-    def test_aceptar_solicitud_exitosa(self):
+    def test_rechazar_solicitud_exitosa(self):
         receptor_username = "usuario1"
         solicitante_username = "usuario2"
 
@@ -13,23 +13,13 @@ class TestGestorUsuarios(BaseTestClass):
                   (receptor_username, solicitante_username))
 
         # Realizar la acción que se está probando
-        res = self.client.post('/aceptar_solicitud', data={'solicitante': solicitante_username})
+        res = self.client.post('/rechazar_solicitud', data={'solicitante': solicitante_username})
 
         # Verificar que la solicitud se haya eliminado
         existing_request = self.db.select("SELECT * FROM Solicitud WHERE usuarioReceptor = ? AND usuarioEnvia = ?",
                                      (receptor_username, solicitante_username))
         self.assertEqual(0, len(existing_request))
 
-        # Verificar que la relación de amistad se haya establecido correctamente
-        friends = self.db.select("SELECT * FROM Amigo WHERE usuarioA = ? AND usuarioB = ?",
-                                 (receptor_username, solicitante_username))
-        self.assertTrue(any(solicitante_username in friend for friend in friends),
-                        "El usuario solicitante no es un amigo")
-
         # Verificar redirección
         self.assertEqual(res.status_code, 302)  # Código de estado de redirección
         self.assertEqual(res.location, '/solicitudes')  # Dirección de redirección esperada
-
-        #Borramos la amistad de la bd para limpiar
-        self.db.delete("DELETE FROM Amigo WHERE usuarioA=? AND usuarioB=?",
-                  (receptor_username, solicitante_username))
